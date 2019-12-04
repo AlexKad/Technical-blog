800db6a27f25465c2f5106a148b5fdacefbdf26e
Alex Orlova <akad.alex@gmail.com>
2019-12-04 13:44:57 -0800

    Update bootstrap, remove unused files

--- assets/custom/js/main.js
+++ assets/custom/js/main.js
@@ -35,4 +35,3 @@ $('#back-to-top').click(function (e) {
   sr.reveal('.step-3', {...step, origin: 'right'});
   sr.reveal('.step-4', {...step, origin: 'left'});
   sr.reveal('.step-5', {...step, origin: 'right'});
-  sr.reveal('.step-6', {...step, origin: 'left'});
